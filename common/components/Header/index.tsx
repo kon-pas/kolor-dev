3cebe479d051ddd852459104c1905dfb42a7a4c4
kon-pas <konpas@mail.com>
2022-08-21 20:01:04 +0200

    Install mui

--- common/components/Header/index.tsx
+++ common/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import styles from './Header.module.scss';
+import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
   return (
-    <header className={styles.header}>
-      Header
+    <header className={styles['header']}>
+      <h1>Kolor</h1>
+      <div className={styles['header__burger-container']}>
+        <MenuIcon />
+      </div>
     </header>
   )
 }
