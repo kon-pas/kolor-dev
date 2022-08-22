347b96c046a9e5dcccb8c83365e8c2160366427c
kon-pas <konpas@mail.com>
2022-08-22 22:18:39 +0200

    Remove mui icon

--- common/components/Header/index.tsx
+++ common/components/Header/index.tsx
@@ -1,14 +1,10 @@
 import styles from './Header.module.scss';
-import MenuIcon from '@mui/icons-material/Menu';
 
 export default function Header() {
   return (
     <header className={styles['header']}>
       <h1>Kolor</h1>
       <div className={styles['header__burger-wrapper']}>
-        <MenuIcon
-          className={styles['header__burger-wrapper__icon']}
-        />
       </div>
     </header>
   )
