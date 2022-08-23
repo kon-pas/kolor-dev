46397e958f14ee56eafdfa02c40765277ddb43af
kon-pas <konpas@mail.com>
2022-08-23 23:20:11 +0200

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
     "./components/**/*.tsx",
   ],
   theme: {
+    colors: {
+      white: '#ffffff',
+      gray: {
+        light: '#f5f5f5',
+        medium: '#dcdcdc',
+        dark: '#8C8889'
+      },
+      black: '#222222'
+    },
     extend: {},
   },
   plugins: [],
