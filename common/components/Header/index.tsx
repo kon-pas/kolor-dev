d4b472d7ea4f82f8884b1b38d0bef5c5f5b92cb8
kon-pas <konpas@mail.com>
2022-08-24 22:21:20 +0200

    Edit navbar

--- common/components/Header/index.tsx
+++ common/components/Header/index.tsx
@@ -11,13 +11,18 @@ export default function Header() {
         </a>
 
         <button className={styles['header__menu--mobile']}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <svg 
+            className="w-6 h-6"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none" viewBox="0 0 24 24"
+            strokeWidth={1.5} stroke="currentColor"
+          >
             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
           </svg>
 
           <span></span>
         </button>
-        
+
         <ul className={styles['header__menu--desktop']}>
           <li className={styles['menu-item']}>
             <a>Item 1</a>
