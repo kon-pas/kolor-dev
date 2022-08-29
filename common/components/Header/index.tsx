937809aa309e9f1860fcda66f83668ab6bb8d15b
kon-pas <konpas@mail.com>
2022-08-29 11:32:37 +0200

    Improve  a11y

--- common/components/Header/index.tsx
+++ common/components/Header/index.tsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className={clsx(styles['content-container'], styles['header__top'])}>
         <a className={styles['header__title']}>
           <div className={styles['header__logo']}>
-            <IconSVG>
+            <IconSVG title="Kolor logo">
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -21,7 +21,7 @@ const Header = () => {
         </a>
 
         <button className={clsx(styles['header__burger'], styles['icon'])}>
-          <IconSVG>
+          <IconSVG title="Open menu">
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
--- common/components/IconSVG/index.tsx
+++ common/components/IconSVG/index.tsx
@@ -2,33 +2,29 @@ import styles from './IconSVG.module.scss';
 
 interface IconSVGProps {
   children: React.ReactNode,
+  strokeWidth?: number
   title?: string,
-  desc?: string
+  desc?: string,
 }
 
-const IconSVG = ({children: path, title, desc }: IconSVGProps) => {
+const IconSVG = ({children: path, strokeWidth, title, desc}: IconSVGProps) => {
   return (
     <svg
       className={styles['icon-svg']}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      strokeWidth={1.75}
+      strokeWidth={strokeWidth ?? 1.75}
       stroke="currentColor"
       role="img"
       aria-hidden={title ? false : true}
+      aria-label={`${title ?? null} ${desc ?? null}`}
       aria-labelledby={`${title ? 'titleID' : null} ${desc ? 'descID' : null}`}
     >
       {title && <title id="titleID">{title}</title>}
       {desc && <desc id="descID">{desc}</desc>}
       {path}
     </svg>
-
-    // <span className={styles['icon__label']}>Open menu</span>
-
-    // <div className={clsx(styles['icon'], className)}>
-    //   {children}
-    // </div>
   )
 }
 
