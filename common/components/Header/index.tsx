27d82ecd15864ea0eb7aab117eae8a01762aa489
kon-pas <konpas@mail.com>
2022-08-29 11:33:47 +0200

    Use IconSVG

--- common/components/Header/index.tsx
+++ common/components/Header/index.tsx
@@ -28,22 +28,6 @@ const Header = () => {
               d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
             />
           </IconSVG>
-          {/* <svg
-            className={styles['icon__svg']}
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.75}
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
-
-          <span className={styles['icon__label']}>Open menu</span> */}
         </button>
         
         <nav className={clsx(styles['header__nav'], styles['header__nav--desktop'])}>
