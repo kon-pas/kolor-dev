7398833116b3e4fa94cb0f7b2b92040bbe746867
kon-pas <konpas@mail.com>
2022-08-31 22:35:55 +0200

    Add Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import Layout from '@/components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <>
+    <Head>
+      <title>Kolor</title>
+    </Head>
+    
     <Layout>
       <Component {...pageProps} />
     </Layout>
+    </>
   )
 }
 
