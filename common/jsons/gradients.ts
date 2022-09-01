f2265017effdb64849d0b9c3ac0a720875f5b936
kon-pas <konpas@mail.com>
2022-09-01 05:07:20 +0200

    Update gradients

--- common/jsons/gradients.ts
+++ common/jsons/gradients.ts
@@ -1,8 +1,37 @@
 import { Gradient } from '@interfaces';
 
-export const GRADIENTS: [Gradient] = [
+export const GRADIENTS: Gradient[] = [
   {
-    colors: ['#EC7272', '#C3FF99'],
-    title: "What is love?"
-  }
+    colors: ["#C3FF99", "#EC7272"],
+    title: "Memories of Spring"
+  },
+  {
+    colors: ["#FFF5E4", "#EE6983"],
+    title: "Strawberry Ice Creams"
+  },
+  {
+    colors: ["#6FEDD6", "#FF9551", "#FF4A4A"],
+    title: "Sweet Popsicle"
+  },
+  {
+    colors: ["#FFF38C", "#C0B236"],
+    title: "Shades of Lemon"
+  },
+  {
+    colors: ["#F7F7F7", "#FFE61B", "#B5FE83"],
+    title: "Citrus Sorbet"
+  },
+  {
+    colors: ["#1C0A00", "#603601", "#CC9544"],
+    title: "Dark Chocolate"
+  },
+  {
+    colors: ["#C1F4C5", "#FFBED8"],
+    title: "Rasberry"
+  },
+  {
+    colors: ["#EAE509", "#7DCE13"],
+    title: "Ripe Lime"
+  },
+  
 ];
