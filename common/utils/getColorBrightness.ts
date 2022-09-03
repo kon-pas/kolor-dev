2cd4afd7d8f2c579e9d2f915f4c00dbc2a88460d
kon-pas <konpas@mail.com>
2022-09-03 23:44:21 +0200

    Swap homebrew function to third-party colvertize

--- common/utils/getColorBrightness.ts
+++ common/utils/getColorBrightness.ts
@@ -1,10 +1,7 @@
-// function getColorBrightness (color: string) {
-//   var c = color.substring(1);      // strip #
-//   var rgb = parseInt(c, 16);   // convert rrggbb to decimal
-//   var r = (rgb >> 16) & 0xff;
-//   var g = (rgb >>  8) & 0xff;
-//   var b = (rgb >>  0) & 0xff;
-//   return  0.2126 * r + 0.7152 * g + 0.0722 * b;
-// }
+import { convert } from 'colvertize';
 
-// export default getColorBrightness;
+function getColorBrightness (color: string): number {
+  return convert(color, 'hsl').l;
+}
+
+export default getColorBrightness;
