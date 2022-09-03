e470601111cd210cef291b0267e1b8959d5e57b8
kon-pas <konpas@mail.com>
2022-09-03 23:42:23 +0200

    Install colvertize

--- common/utils/getColorBrightness.ts
+++ common/utils/getColorBrightness.ts
@@ -1,11 +1,10 @@
-function getColorBrightness (color: string) {
-  var c = color.substring(1);      // strip #
-  var rgb = parseInt(c, 16);   // convert rrggbb to decimal
-  // var r = (rgb >> 16) & 0xff;
-  // var g = (rgb >>  8) & 0xff;
-  var b = (rgb >>  0) & 0xff;
-  // return  0.2126 * r + 0.7152 * g + 0.0722 * b;
-  return  0.0722 * b;
-}
+// function getColorBrightness (color: string) {
+//   var c = color.substring(1);      // strip #
+//   var rgb = parseInt(c, 16);   // convert rrggbb to decimal
+//   var r = (rgb >> 16) & 0xff;
+//   var g = (rgb >>  8) & 0xff;
+//   var b = (rgb >>  0) & 0xff;
+//   return  0.2126 * r + 0.7152 * g + 0.0722 * b;
+// }
 
-export default getColorBrightness;
+// export default getColorBrightness;
