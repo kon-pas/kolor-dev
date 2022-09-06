525dffd61e339196285d7781fc4a14bef27a5f36
kon-pas <konpas@mail.com>
2022-09-06 03:55:44 +0200

    Improve section header RWD

--- common/components/Header/index.tsx
+++ common/components/Header/index.tsx
@@ -10,7 +10,7 @@ const Header = () => {
         <a className={styles['header__title']}>
           <div className={styles['header__logo']}>
             <Image 
-              src="/assets/svgs/kolor_logo_cube_1.svg"
+              src="/assets/svgs/kolor_logo_cube.svg"
               alt="Kolor Cube"
               height={40}
               width={40}
