97750b6ba4915b171611c7f0a955ab2227544f8e
kon-pas <konpas@mail.com>
2022-09-06 11:44:36 +0200

    Add redirects the proper way

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true
+  swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/gradient',
+        destination: '/gradients',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
