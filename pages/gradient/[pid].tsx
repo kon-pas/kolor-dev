500836baa4faae99f08df4f0bf249e03a580757a
kon-pas <konpas@mail.com>
2022-09-07 15:56:33 +0200

    Cleanup

--- pages/gradient/[pid].tsx
+++ pages/gradient/[pid].tsx
@@ -32,12 +32,12 @@ const GradientPid: NextPage<GradientPidProps> = ({
 
       <div className={styles['buttons']}>
         <div className={styles['buttons__left']}>
-          <Button label='Like'>
+          <Button label='Save'>
             <IconSVG>
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
-                d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
               />
             </IconSVG>
           </Button>
