bd9b8a8f43c4a5ee4fb14fcabb1aa7e59317f950
kon-pas <konpas@mail.com>
2022-09-07 08:56:59 +0200

    Small refactor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,21 @@
-import '@styles/globals.scss'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
-import Layout from '@components/Layout'
+import '@styles/globals.scss';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import Layout from '@components/Layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
-      <title>Kolor</title>
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
+      <Head>
+        <title>Kolor</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
 
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   )
 }
 
-export default MyApp
+export default MyApp;
