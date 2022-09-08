41ce78f294bb9a3ab569d9c8a4e0fb873373ce2d
kon-pas <konpas@mail.com>
2022-09-08 20:44:47 +0200

    Init

---
+++ common/components/Tag/index.tsx
@@ -0,0 +1,27 @@
+import styles from 'Tag.module.scss';
+
+import type { MainColors } from '@enums';
+ 
+interface TagProps {
+  children?: React.ReactNode;
+  type?: 'hash' | 'color';
+  color: MainColors;
+  onClick?: () => void;
+}
+
+const Tag: React.FC<TagProps> = ({children, type, color, onClick}) => {
+  return (
+    <div
+      className={styles.tag}
+      onClick={onClick}
+    >
+      { type === 'color' && color }
+      <span className={styles.tag__label}>
+        { type === 'hash' && '#' }
+        { children }
+      </span>
+    </div>
+  )
+}
+
+export default Tag;
