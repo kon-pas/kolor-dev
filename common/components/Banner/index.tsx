c39f4f9e5f54acc78d632083f430039287e332e8
kon-pas <konpas@mail.com>
2022-09-13 23:16:11 +0200

    Init

---
+++ common/components/Banner/index.tsx
@@ -0,0 +1,15 @@
+import styles from './Banner.module.scss';
+
+interface BannerProps {
+
+}
+
+const Banner: BannerProps = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default Banner;
