98925fd66af3701a3807108ea7f0f080a285fbbe
kon-pas <konpas@mail.com>
2022-09-14 23:43:33 +0200

    Misc

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,11 @@ import styles from '@styles/pages/Home.module.scss';
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div className={styles["home-page"]}>Home</div>;
+  return (
+    <div className={styles["home-page"]}>
+      dsa
+    </div>
+  );
 };
 
 export default Home;
