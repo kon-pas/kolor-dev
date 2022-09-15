5cb4c0cb3396897a31266427fcfc826aa1464f86
kon-pas <konpas@mail.com>
2022-09-15 17:46:16 +0200

    Put main colors into constants

---
+++ common/constants/main-colors.ts
@@ -0,0 +1,11 @@
+import type { HexColor } from '@types';
+
+const MAIN_COLORS: {
+  [key: string]: HexColor
+} = {
+  cyan: '#2A9BC5',
+  magenta: '#F47EBD',
+  yellow: '#F2C89A'
+}
+
+export default MAIN_COLORS;
