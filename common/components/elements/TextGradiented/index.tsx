e8f57dd661e2ecd93d1575d3e844f789c4381b3f
kon-pas <konpas@mail.com>
2022-09-15 19:36:51 +0200

    Add direction

--- common/components/elements/TextGradiented/index.tsx
+++ common/components/elements/TextGradiented/index.tsx
@@ -1,19 +1,23 @@
 import styles from './TextGradiented.module.scss';
 
-import type { GradientHue } from '@types';
+import type { GradientHue, EightDirections } from '@types';
 
 interface TextGradientedProps {
   colors: GradientHue;
   children: string;
-  direction?: string;
+  direction?: EightDirections;
 }
 
-const TextGradiented: React.FC<TextGradientedProps> = ({ children: text, colors }) => {
+const TextGradiented: React.FC<TextGradientedProps> = ({
+  children: text,
+  colors,
+  direction
+}) => {
   return(
     <span 
       className={styles['text-gradiented']}
       style={{ 
-        backgroundImage: `linear-gradient(to right, ${colors.join(', ')})`
+        backgroundImage: `linear-gradient(to ${direction ?? 'right'}, ${colors.join(', ')})`
       }}
     >
       {text}
