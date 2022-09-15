7143412023f9c2c1e36f8c94a5e813ad580865a3
kon-pas <konpas@mail.com>
2022-09-15 22:40:46 +0200

    Text roll animation

--- common/components/elements/TextAnimated/index.tsx
+++ common/components/elements/TextAnimated/index.tsx
@@ -1,5 +1,7 @@
 import styles from './TextAnimated.module.scss';
 
+import { useState, useEffect } from 'react';
+
 interface TextAnimatedProps {
   labels: string[];
 }
@@ -7,14 +9,36 @@ interface TextAnimatedProps {
 const TextAnimated: React.FC<TextAnimatedProps> = ({
   labels
 }) => {
+  
+  const [labelIndex, setLabelIndex] = useState(0);
+  const [currentLabel, setCurrentLabel] = useState(labels[0]);
+
+  useEffect(() => {
+    setCurrentLabel(labels[labelIndex])
+  }, [labelIndex, labels]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+
+
+      console.log('Works');
+
+
+    }, 3000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <span className={styles['text-animated']}>
-      { labels.map((label, idx) =>
+
+      <span className={styles['text-animated__space-filler']}>{currentLabel}</span>
+
+      {labels.map((label, idx) =>
         <span
-          className={styles['label']}
+          className={styles['text-animated__label']}
           key={idx}
         >
-          {label}
+          {'your work'}
         </span>
       )}
     </span>
