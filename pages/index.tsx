25cf51a24c57c65d83ce2470b4cd94e4f970880e
kon-pas <konpas@mail.com>
2022-09-15 18:28:44 +0200

    Add home page header

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,69 +9,65 @@ import TextGradiented from '@components/elements/TextGradiented';
 const Home: NextPage = () => {
   return (
     <div className={styles["home-page"]}>
-      <div className={styles["home-page__content"]}>
-        <header className={styles['header']}>
-          <h1 className={styles['header__heading']}>
-            <strong>
-              <TextGradiented
-                colors={[MAIN_COLORS.C, MAIN_COLORS.M, MAIN_COLORS.Y]}
-              >
-                Kolor
-              </TextGradiented>
-            </strong>
+      <header className={styles['header']}>
+        <h1 className={styles['header__heading']}>
+          <strong>
+            <TextGradiented
+              colors={[MAIN_COLORS.C, MAIN_COLORS.M, MAIN_COLORS.Y]}
+            >
+              Kolor
+            </TextGradiented>
+          </strong>
 
-            <span>&nbsp;</span>
+          <span>&nbsp;</span>
 
-            <span>your work</span>
-            {/* <span>with ease</span>
-            <span>for free</span> */}
+          <span>your work</span>
+          {/* <span>with ease</span>
+          <span>for free</span> */}
 
-            <span>!</span>
+          <span>!</span>
+        </h1>
+      </header>
+      
+      {/* <div>
+        <header>
+          <h1>
+            25
           </h1>
+          <h2>
+            Gradients
+          </h2>
         </header>
-
-        {/* <div>
-          <header>
-            <h1>
-              25
-            </h1>
-            <h2>
-              Gradients
-            </h2>
-          </header>
-          <p>
-            Carefully selected
-          </p>
-        </div>
-
-        <div>
-          <header>
-            <h1>
-              30
-            </h1>
-            <h2>
-              Palletes
-            </h2>
-          </header>
-          <p>
-            Carefully selected
-          </p>
-        </div>
-
-        <div>
-          <header>
-            <h1>
-              35
-            </h1>
-            <h2>
-              Colors
-            </h2>
-          </header>
-          <p>
-            Carefully selected
-          </p>
-        </div> */}
+        <p>
+          Carefully selected
+        </p>
       </div>
+      <div>
+        <header>
+          <h1>
+            30
+          </h1>
+          <h2>
+            Palletes
+          </h2>
+        </header>
+        <p>
+          Carefully selected
+        </p>
+      </div>
+      <div>
+        <header>
+          <h1>
+            35
+          </h1>
+          <h2>
+            Colors
+          </h2>
+        </header>
+        <p>
+          Carefully selected
+        </p>
+      </div> */}
     </div>
   );
 };
