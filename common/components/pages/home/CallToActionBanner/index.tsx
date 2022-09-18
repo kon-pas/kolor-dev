f67f5c9092ac1d85187d68353561c64979d131e7
kon-pas <konpas@mail.com>
2022-09-18 22:51:09 +0200

    Misc

--- common/components/pages/home/CallToActionBanner/index.tsx
+++ common/components/pages/home/CallToActionBanner/index.tsx
@@ -3,6 +3,7 @@ import styles from './CallToActionBanner.module.scss';
 interface CallToActionBannerProps {
   title: string;
   subtitle: string;
+  desc: string;
   color?: string;
 }
 
@@ -14,10 +15,10 @@ const CallToActionBanner: React.FC<CallToActionBannerProps> = ({
     <article className={styles['call-to-action-banner']}>
       <header className={styles['call-to-action-banner__header']}>
         <h1 className={styles['call-to-action-banner__heading-1']}>
-          30
+          {title}
         </h1>
         <h2 className={styles['call-to-action-banner__heading-2']}>
-          Palletes
+          {subtitle}
         </h2>
       </header>
       <p className={styles['call-to-action-banner__paragraph']}>
