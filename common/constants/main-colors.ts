57cb4070392eabd241b4a202ea0efadb9ebe77c5
kon-pas <konpas@mail.com>
2022-09-20 21:18:39 +0200

    New main colors

--- common/constants/main-colors.ts
+++ common/constants/main-colors.ts
@@ -3,9 +3,9 @@ import type { HexColor } from '@types';
 const MAIN_COLORS: {
   [key: string]: HexColor
 } = {
-  C: '#2A9BC5',
-  M: '#F47EBD',
-  Y: '#F2C89A'
+  C: '#0f84c9',
+  M: '#ff85cf',
+  Y: '#ffe9a1'
 }
 
 export default MAIN_COLORS;
