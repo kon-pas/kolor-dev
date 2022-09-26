651b8d1a631bd375d355d76cbb2f4ea05128b4fc
kon-pas <konpas@mail.com>
2022-09-26 23:06:51 +0200

    Cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -93,29 +93,6 @@ const Home: NextPage = () => {
               Kolor
             </TextUnderlined>
           </strong>
-
-          <strong className={styles['header__text-underlined--desktop']}>
-            <span className={styles['header__text-underlined--desktop__text']}>
-              Kolor
-            </span>
-
-            <div className={styles['header__text-underlined--desktop__line']}>
-              <GradientBackground colors={[MAIN_COLORS.C, MAIN_COLORS.M, MAIN_COLORS.Y]}/>
-            </div>
-          </strong>
-
-          <div className={styles['header__animated-text']}>
-            <TextAnimated labels={['your work', 'with ease', 'for free']} />
-
-            <span className={styles['header__dot']}>
-              <TextGradiented
-                colors={[MAIN_COLORS.C, MAIN_COLORS.M, MAIN_COLORS.Y]}
-                to='top left'
-              >
-                .
-              </TextGradiented>
-            </span>
-          </div>
         </h1>
       </header> */}
 
