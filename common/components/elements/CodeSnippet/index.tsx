8f96eb2b2484b76481b97d6e6ee28316e6425628
kon-pas <konpas@mail.com>
2022-11-16 22:53:22 +0100

    add(`CodeSnippet`): handle `onClick`

--- common/components/elements/CodeSnippet/index.tsx
+++ common/components/elements/CodeSnippet/index.tsx
@@ -3,14 +3,21 @@ import styles from "./CodeSnippet.module.scss";
 interface CodeSnippetProps {
   children: React.ReactNode;
   title?: string;
+  onClick?: () => void;
 }
 
-const CodeSnippet: React.FC<CodeSnippetProps> = ({ children, title }) => {
+const CodeSnippet: React.FC<CodeSnippetProps> = ({
+  children,
+  title,
+  onClick,
+}) => {
   return (
-    <div className={styles["code-snippet"]}>
-      <p className={styles["code-snippet__title"]}>{title}</p>
-
+    <div
+      className={styles["code-snippet"]}
+      onClick={() => onClick && onClick()}
+    >
       <div className={styles["code-snippet__code"]}>{children}</div>
+      <p className={styles["code-snippet__title"]}>{title}</p>
     </div>
   );
 };
