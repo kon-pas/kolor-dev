c52f7eb76fa2c3b52f788daa22be22aedcd50375
kon-pas <konpas@mail.com>
2022-11-18 22:46:32 +0100

    init(contants): `nav-items`

---
+++ common/constants/nav-items.ts
@@ -0,0 +1,26 @@
+import { NavItem } from "@interfaces";
+
+const NAV_ITEMS: NavItem[] = [
+  {
+    label: "Gradients",
+    path: "/gradients",
+  },
+  {
+    label: "Gradient Generator",
+    path: null,
+  },
+  {
+    label: "Color Palletes",
+    path: null,
+  },
+  {
+    label: "Color Generator",
+    path: null,
+  },
+  {
+    label: "Color Finder",
+    path: null,
+  },
+];
+
+export default NAV_ITEMS;
