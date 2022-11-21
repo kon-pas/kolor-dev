410befff52d73a1fd6c6377589337dfbfd33cad7
kon-pas <konpas@mail.com>
2022-11-21 23:29:33 +0100

    feat: handle being inactive

--- common/components/pages/home/CallToActionBanner/index.tsx
+++ common/components/pages/home/CallToActionBanner/index.tsx
@@ -1,17 +1,27 @@
 import styles from "./CallToActionBanner.module.scss";
 
+import clsx from "clsx";
+
 interface CallToActionBannerProps {
   children: React.ReactNode;
   desc: string;
   color?: string;
+  wip?: boolean;
 }
 
 const CallToActionBanner: React.FC<CallToActionBannerProps> = ({
   desc,
   children: title,
+  color,
+  wip,
 }) => {
   return (
-    <article className={styles["call-to-action-banner"]}>
+    <article
+      className={clsx(
+        styles["call-to-action-banner"],
+        styles[wip ? "inactive" : "active"]
+      )}
+    >
       <header className={styles["call-to-action-banner__header"]}>
         <h1 className={styles["call-to-action-banner__heading"]}>{title}</h1>
       </header>
