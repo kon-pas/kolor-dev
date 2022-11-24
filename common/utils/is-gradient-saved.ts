a1a063ce99b690f951deeef439b669a3d9a48f48
kon-pas <konpas@mail.com>
2022-11-24 23:38:09 +0100

    init(utils): `is-gradient-saved`

---
+++ common/utils/is-gradient-saved.ts
@@ -0,0 +1,9 @@
+const isGradientSaved = (id: string): boolean => {
+  const favGradientsStr = localStorage.getItem("gradients");
+
+  if (favGradientsStr) {
+    return JSON.parse(favGradientsStr).includes(id);
+  } else return false;
+};
+
+export default isGradientSaved;
