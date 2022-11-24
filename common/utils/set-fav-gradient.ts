fc6b7820e31fd5d3df89ea0fb2e602197397a956
kon-pas <konpas@mail.com>
2022-11-24 22:43:35 +0100

    init(utils): `set-fav-gradient`

---
+++ common/utils/set-fav-gradient.ts
@@ -0,0 +1,14 @@
+const setFavGradient = (id: string) => {
+  const favGradientsStr = localStorage.getItem("gradients");
+
+  if (favGradientsStr) {
+    const favGradients: string[] = JSON.parse(favGradientsStr);
+
+    if (favGradients.includes(id))
+      localStorage.setItem("gradients", JSON.stringify(favGradients.push(id)));
+  } else {
+    localStorage.setItem("gradients", JSON.stringify([id]));
+  }
+};
+
+export default setFavGradient;
