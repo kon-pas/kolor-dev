f1379a871d4286d8928b1ad3793bec2344aec551
kon-pas <konpas@mail.com>
2022-11-27 23:09:21 +0100

    add: event handlers

--- common/components/pages/gradients/GradientCard/index.tsx
+++ common/components/pages/gradients/GradientCard/index.tsx
@@ -4,12 +4,13 @@ import "react-toastify/dist/ReactToastify.css";
 import type { GradientScheme } from "@interfaces";
 import type { GradientId } from "@types";
 
+import { useState, useEffect } from "react";
 import { useRouter, NextRouter } from "next/router";
-import { toast, ToastContainer, Flip } from "react-toastify";
+import { toast } from "react-toastify";
 
 import { TOAST_OPTIONS } from "@constants";
 import { getCleanHex } from "@utils";
-// import { local } from "@services";
+import { local } from "@services";
 
 import IconSVG from "@components/elements/IconSVG";
 import Gradient from "@components/elements/GradientBackground";
@@ -26,9 +27,15 @@ const GradientCard: React.FC<GradientCardProps> = ({
   gradient,
   gradientId,
 }) => {
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsSaved(gradient ? local.gradients.includes(gradient.id) : false);
+  }, [gradient]);
+
   const router: NextRouter = useRouter();
 
-  const handleRedirect = () => router.push(`/gradient/${gradientId}`);
+  const handleRedirect = () => void router.push(`/gradient/${gradientId}`);
 
   const handleColorOnClick = (color: string) => {
     navigator.clipboard.writeText(color).then(
@@ -39,6 +46,13 @@ const GradientCard: React.FC<GradientCardProps> = ({
         toast("Copy to Clipboard Failed :/", TOAST_OPTIONS);
       }
     );
+  };
+
+  const handleSaveButtonOnClick = () => {
+    const { id } = gradient as GradientScheme;
+    if (local.gradients.includes(id)) local.gradients.remove(id);
+    else local.gradients.add(id);
+    setIsSaved((isSaved) => !isSaved);
   };
 
   return (
@@ -64,32 +78,24 @@ const GradientCard: React.FC<GradientCardProps> = ({
       </div>
 
       <div className={styles["card__desc"]}>
-        <Button label="Save">
-          <IconSVG title="Like gradient" strokeWidth={1}>
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
-            />
-          </IconSVG>
+        <Button label="Save" onClick={handleSaveButtonOnClick}>
+          {isSaved ? (
+            <IconSVG filled title="Like gradient" strokeWidth={1}>
+              <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+            </IconSVG>
+          ) : (
+            <IconSVG title="Like gradient" strokeWidth={1}>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+              />
+            </IconSVG>
+          )}
         </Button>
 
         <Button onClick={handleRedirect} label={gradient.title} />
       </div>
-
-      <ToastContainer
-        position="bottom-center"
-        autoClose={1000}
-        hideProgressBar
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss={false}
-        draggable={false}
-        pauseOnHover
-        transition={Flip}
-        theme="dark"
-      />
     </div>
   );
 };
