271c2248ccea4a1c6a58d0f99b2cd1fac8ffa3af
kon-pas <konpas@mail.com>
2022-11-27 22:27:23 +0100

    feat: handle local storage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,26 @@
 import "@styles/globals.scss";
 
-import Head from "next/head";
-
 import type { AppProps } from "next/app";
+
+import { useEffect } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+import { local } from "@services";
 
 import Layout from "@components/composition/Layout";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const { events } = useRouter();
+
+  useEffect(() => {
+    events.on("routeChangeStart", local.gradients.save);
+
+    return () => {
+      events.off("routeChangeStart", local.gradients.save);
+    };
+  });
+
   return (
     <>
       <Head>
