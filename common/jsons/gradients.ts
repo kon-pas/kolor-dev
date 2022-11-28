a0b970480b4c3b8c3f3d5dcc5e1380aaedffcdec
kon-pas <konpas@mail.com>
2022-11-28 03:16:00 +0100

    add(jsons): fill with actual content

--- common/jsons/gradients.ts
+++ common/jsons/gradients.ts
@@ -1,11 +1,11 @@
 import type { GradientsJSON } from "@interfaces";
-import { customAlphabet } from "nanoid";
 import { MainColors, MiscTags } from "@enums";
+// import { customAlphabet } from "nanoid";
 
-const nanoid = customAlphabet(
-  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
-  8
-);
+// const nanoid = customAlphabet(
+//   "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",
+//   8
+// );
 
 export const gradients: GradientsJSON = [
   {
@@ -13,108 +13,208 @@ export const gradients: GradientsJSON = [
     title: "Faerie's Dream",
     tags: {
       mainColors: [MainColors.Green, MainColors.Blue, MainColors.Purple],
-      misc: [MiscTags.Cold, MiscTags.Magic, MiscTags.Intense],
+      misc: [MiscTags.Magic, MiscTags.Intense],
     },
   },
   {
     colors: ["#60efff", "#00ff87"],
     title: "Morning Breeze",
+    tags: {
+      mainColors: [MainColors.Blue, MainColors.Green],
+      misc: [MiscTags.Intense],
+    },
   },
   {
     colors: ["#BFFF00", "#60F10A"],
     title: "Guilty Lime",
+    tags: {
+      mainColors: [MainColors.Green],
+      misc: [MiscTags.Intense, MiscTags.Mono],
+    },
   },
   {
     colors: ["#f89b29", "#ff0f7b"],
     title: "Ripe Mirabelle",
+    tags: {
+      mainColors: [MainColors.Orange, MainColors.Pink],
+      misc: [MiscTags.Intense, MiscTags.Warm],
+    },
   },
   {
     colors: ["#a9ff68", "#ff8989"],
     title: "Fresh Apple",
+    tags: {
+      mainColors: [MainColors.Green, MainColors.Red],
+      misc: [MiscTags.Warm, MiscTags.Varied],
+    },
   },
   {
     colors: ["#9bafd9", "#103783"],
     title: "Navy Blue",
+    tags: {
+      mainColors: [MainColors.Blue],
+      misc: [MiscTags.Cold, MiscTags.Mono],
+    },
   },
   {
     colors: ["#f8f9c7", "#f2c1ea", "#f67b6b"],
     title: "Sweet Popsicle",
+    tags: {
+      mainColors: [MainColors.Yellow, MainColors.Pink, MainColors.Orange],
+      misc: [MiscTags.Calm, MiscTags.Varied, MiscTags.Magic],
+    },
   },
   {
     colors: ["#d3eef4", "#f1eec8", "#f3a46c"],
     title: "Wet Sand",
+    tags: {
+      mainColors: [MainColors.Blue, MainColors.Yellow, MainColors.Orange],
+      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Varied],
+    },
   },
   {
     colors: ["#FFFF8B", "#f19e18", "#e62314"],
     title: "Red Fox's Tail",
+    tags: {
+      mainColors: [MainColors.Yellow, MainColors.Orange, MainColors.Red],
+      misc: [MiscTags.Intense, MiscTags.Warm],
+    },
   },
   {
     colors: ["#fff95b", "#ff930f"],
     title: "Amber Gold",
+    tags: {
+      mainColors: [MainColors.Yellow, MainColors.Orange],
+      misc: [MiscTags.Intense, MiscTags.Warm],
+    },
   },
   {
     colors: ["#f3f520", "#59d102"],
     title: "Freshly Cut Grass",
+    tags: {
+      mainColors: [MainColors.Yellow, MainColors.Green],
+      misc: [MiscTags.Intense],
+    },
   },
   {
     colors: ["#ebf4f5", "#b5c6e0"],
     title: "Cold Paper",
+    tags: {
+      mainColors: [MainColors.White, MainColors.Gray],
+      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Cold, MiscTags.Mono],
+    },
   },
   {
     colors: ["#fbe9d7", "#f6d5f7"],
     title: "Warm Paper",
+    tags: {
+      mainColors: [MainColors.Pink],
+      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Mono],
+    },
   },
   {
     colors: ["#82f4b1", "#30c67c"],
     title: "Faded Lime",
+    tags: {
+      mainColors: [MainColors.Green],
+      misc: [MiscTags.Cold, MiscTags.Mono],
+    },
   },
   {
     colors: ["#751006", "#1f0021"],
     title: "Bloody Black",
+    tags: {
+      mainColors: [MainColors.Red, MainColors.Black],
+      misc: [MiscTags.Dark, MiscTags.Mono],
+    },
   },
   {
     colors: ["#00458e", "#000328"],
     title: "Oceans' Depths",
+    tags: {
+      mainColors: [MainColors.Blue, MainColors.Black],
+      misc: [MiscTags.Dark, MiscTags.Cold, MiscTags.Mono],
+    },
   },
   {
     colors: ["#e4e95d", "#e89d1f"],
     title: "Shades of Lemon",
+    tags: {
+      mainColors: [MainColors.Yellow],
+      misc: [MiscTags.Intense, MiscTags.Mono],
+    },
   },
   {
     colors: ["#affcaf", "#12dff3"],
     title: "Hawaiian Shallows",
+    tags: {
+      mainColors: [MainColors.Blue],
+      misc: [MiscTags.Intense, MiscTags.Cold, MiscTags.Mono],
+    },
   },
   {
     colors: ["#eef2f3", "#8399a2"],
     title: "Tabula Rasa",
+    tags: {
+      mainColors: [MainColors.White, MainColors.Gray],
+      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Cold, MiscTags.Mono],
+    },
   },
   {
     colors: ["#c5f9d7", "#f7d486", "#f27a7d"],
     title: "Ising Model",
+    tags: {
+      mainColors: [MainColors.Blue, MainColors.Yellow, MainColors.Red],
+      misc: [MiscTags.Varied],
+    },
   },
   {
     colors: ["#f8dadc", "#b6edc8", "#eeacdc"],
     title: "First Days of Spring",
+    tags: {
+      mainColors: [MainColors.Pink, MainColors.Green, MainColors.Purple],
+      misc: [MiscTags.Light, MiscTags.Magic, MiscTags.Varied],
+    },
   },
   {
     colors: ["#9AEEA2", "#f7f065", "#FD9891"],
     title: "Middle of Summer",
+    tags: {
+      mainColors: [MainColors.Green, MainColors.Yellow, MainColors.Red],
+      misc: [MiscTags.Intense, MiscTags.Varied],
+    },
   },
   {
     colors: ["#e3e3e3", "#eb8a3d"],
     title: "Autumn's Peace",
+    tags: {
+      mainColors: [MainColors.Gray, MainColors.Orange],
+      misc: [MiscTags.Calm],
+    },
   },
   {
     colors: ["#eeddf3", "#FDB5BE", "#6b8080"],
     title: "Warmth of Winter",
+    tags: {
+      mainColors: [MainColors.Pink, MainColors.Gray],
+      misc: [MiscTags.Intense, MiscTags.Varied],
+    },
   },
   {
     colors: ["#c621e5", "#7d7cf9"],
     title: "Scent of Lavender",
+    tags: {
+      mainColors: [MainColors.Purple],
+      misc: [MiscTags.Magic, MiscTags.Intense, MiscTags.Mono],
+    },
   },
   {
     colors: ["#000000", "#ffffff"],
     title: "Black & White",
+    tags: {
+      mainColors: [MainColors.Black, MainColors.White],
+      misc: [MiscTags.Light, MiscTags.Dark],
+    },
   },
 ]
   .map((gradient, idx) => ({
