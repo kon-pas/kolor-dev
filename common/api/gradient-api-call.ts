42aaaba0bd893da92c0168a1728b91aa7ca0960d
kon-pas <konpas@mail.com>
2022-12-07 23:25:40 +0100

    init(api): `gradient-api-call`

---
+++ common/api/gradient-api-call.ts
@@ -0,0 +1,26 @@
+import type { ApiResponse } from "@interfaces";
+
+import { gradients } from "@database";
+
+const gradientApiCAll = async (pid: number): Promise<ApiResponse> =>
+  pid in gradients
+    ? {
+        status: 200,
+        statusText: "OK",
+        ok: true,
+        body: JSON.stringify(gradients[pid]),
+        async json() {
+          return JSON.parse(JSON.stringify(gradients[pid]));
+        },
+      }
+    : {
+        status: 404,
+        statusText: "Not Found",
+        ok: false,
+        body: null,
+        async json() {
+          return null;
+        },
+      };
+
+export default gradientApiCAll;
