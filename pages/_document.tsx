74edf1c952916a1db4e6e072b675f5d3e4719a34
kon-pas <konpas@mail.com>
2022-12-07 23:17:30 +0100

    add(`_document`): `link`s & `meta`

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,26 +1,24 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link
-          href="http://fonts.cdnfonts.com/css/satoshi"
-          rel="stylesheet"
-        />
-        <link
-          href="https://rsms.me/inter/inter.css"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.cdnfonts.com/css/satoshi" rel="stylesheet" />
+        <link href="https://rsms.me/inter/inter.css" rel="stylesheet" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
           rel="stylesheet"
         />
+        <meta
+          name="description"
+          content="Kolor is a kit of color-related tools and sets for Artists, Designes & Developers."
+        ></meta>
       </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
