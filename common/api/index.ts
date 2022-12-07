c467682f1839248cd805d4abc4149fdd9b15ab18
kon-pas <konpas@mail.com>
2022-12-07 23:16:43 +0100

    init(commons): api

---
+++ common/api/index.ts
@@ -0,0 +1,5 @@
+// @@@ NOTE: Imitating external API. Cannot fetch data from internet API.
+
+export { default as gradientsApiCall } from "./gradients-api-call";
+
+export { default as gradientApiCall } from "./gradient-api-call";
