9bb7f4c8526bc5c981bbc5b8a21b0a81416ce32d
kon-pas <konpas@mail.com>
2022-12-10 05:16:07 +0100

    feat(api): deprecate

--- pages/api/gradient/[pid].tsx
+++ pages/api/gradient/[pid].tsx
@@ -1,14 +1,18 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-import { gradients } from "@database";
-import { GradientScheme } from "@interfaces";
+// import { gradients } from "@database";
+// import { GradientScheme } from "@interfaces";
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<GradientScheme | string>
-) {
-  const pid = req.query.pid as string;
+// export default function handler(
+//   req: NextApiRequest,
+//   res: NextApiResponse<GradientScheme | string>
+// ) {
+//   const pid = req.query.pid as string;
 
-  if (pid in gradients) res.status(200).json(gradients[pid]);
-  else res.status(404).send(`PID Not Found: ${pid}`);
-}
+//   if (pid in gradients) res.status(200).json(gradients[pid]);
+//   else res.status(404).send(`PID Not Found: ${pid}`);
+// }
+
+// @@@ DEPRECATED: Delete this API route.
+// @@@ NOTE: Use `@api` instead.
+export {};
--- pages/api/gradients.tsx
+++ pages/api/gradients.tsx
@@ -1,11 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+// import type { NextApiRequest, NextApiResponse } from "next";
 
-import { gradients } from "@database";
-import { GradientsJSON } from "@interfaces";
+// import { gradients } from "@database";
+// import { GradientsJSON } from "@interfaces";
 
-export default function handler(
-  _: NextApiRequest,
-  res: NextApiResponse<GradientsJSON>
-) {
-  res.status(200).json(gradients);
-}
+// export default function handler(
+//   _: NextApiRequest,
+//   res: NextApiResponse<GradientsJSON>
+// ) {
+//   res.status(200).json(gradients);
+// }
+
+// @@@ DEPRECATED: Delete this API route.
+// @@@ NOTE: Use `@api` instead.
+export {};
