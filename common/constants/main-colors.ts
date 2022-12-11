3e7d26f99b735f575661d5c1eadd04937582e659
kon-pas <konpas@mail.com>
2022-12-11 20:46:38 +0100

    add(constants): new main colors

--- common/constants/main-colors.ts
+++ common/constants/main-colors.ts
@@ -1,11 +1,19 @@
+import type { MainColors } from "@enums";
 import type { HexColor } from "@types";
 
-const MAIN_COLORS: {
-  [key: string]: HexColor;
-} = {
-  C: "#0f84c9",
-  M: "#ff85cf",
-  Y: "#ffe9a1",
+const MAIN_COLORS: { [key in MainColors]: HexColor } = {
+  Blue: "#51A9FF",
+  Cyan: "#03CEAC",
+  Green: "#97DE40",
+  Yellow: "#FFC933",
+  Orange: "#FB943D",
+  Red: "#F05050",
+  Purple: "#B898E5",
+  Pink: "#FF8AF0",
+  White: "#FFFFFF",
+  Gray: "#A9B4BF",
+  Black: "#000000",
+  Brown: "#513927",
 };
 
 export default MAIN_COLORS;
