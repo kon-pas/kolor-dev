8a63c896866fda0a9bc875ffb434d8dab0ffc704
kon-pas <konpas@mail.com>
2022-12-11 20:46:13 +0100

    init(constants): `BRAND_COLORS`

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -1,3 +1,5 @@
-export { default as MAIN_COLORS } from "./main-colors";
+export { default as BRAND_COLORS } from "./brand-colors";
 
 export { default as TOAST_OPTIONS } from "./toast-options";
+
+export { default as MAIN_COLORS } from "./main-colors";
