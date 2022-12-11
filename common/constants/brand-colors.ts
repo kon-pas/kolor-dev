ff66d8117e85f7ca7dd8007d0ead203079d58968
kon-pas <konpas@mail.com>
2022-12-11 20:07:01 +0100

    refactor: rename `MAIN_COLORS` to `BRAND_COLORS`

---
+++ common/constants/brand-colors.ts
@@ -0,0 +1,11 @@
+import type { HexColor } from "@types";
+
+const BRAND_COLORS: {
+  [key: string]: HexColor;
+} = {
+  C: "#0f84c9",
+  M: "#ff85cf",
+  Y: "#ffe9a1",
+};
+
+export default BRAND_COLORS;
