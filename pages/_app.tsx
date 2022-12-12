b52dcf1443078ce15d2b76bb6eea4ada5dbc482b
kon-pas <konpas@mail.com>
2022-12-12 08:30:13 +0100

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <Loader />
+      <Loader /> 
 
       <PathContextManager.PathContextProvider>
         <Layout>
