127fa66e0b39ffa6de887108c7f22de2bc372081
kon-pas <konpas@mail.com>
2022-12-12 07:48:59 +0100

    add(`_app`): `PathContext`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
+import { PathContextManager } from "@contexts";
 
 import { local } from "@services";
 import Layout from "@components/composition/Layout";
@@ -27,9 +28,11 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
 
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <PathContextManager.PathContextProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </PathContextManager.PathContextProvider>
     </>
   );
 }
