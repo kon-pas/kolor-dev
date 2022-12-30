77a6013709c11f1267751a855cab2728d2775ed8
kon-pas <konpas@mail.com>
2022-12-30 23:31:19 +0100

    add(`/gradients`): search form

--- pages/gradients.tsx
+++ pages/gradients.tsx
@@ -2,13 +2,15 @@ import styles from "@styles/pages/gradients.module.scss";
 
 import type { NextPage, GetStaticProps } from "next";
 import type { GradientScheme } from "@types";
-import { useEffect } from "react";
 
+import { useEffect } from "react";
 import { prisma } from "@lib";
 import { usePath } from "@hooks";
+import { MISC_TAGS, MAIN_COLORS } from "@constants";
 
 import GradientCard from "@components/pages/gradients/GradientCard";
 import TextUnderlined from "@components/elements/TextUnderlined";
+import Tag from "@components/elements/Tag";
 
 interface GradientsProps {
   gradients: GradientScheme[];
@@ -48,6 +50,30 @@ const Gradients: NextPage<GradientsProps> = ({ gradients }) => {
           <span>.</span>
         </p>
       </header>
+
+      <div className={styles["form"]}>
+        <input
+          className={styles["form__input"]}
+          type="text"
+          placeholder="Name"
+        />
+
+        <div className={styles["form__tags"]}>
+          {Object.values(MAIN_COLORS).map((color, idx) => (
+            <Tag type="color" color={color} key={idx}>
+              {color}
+            </Tag>
+          ))}
+        </div>
+
+        <div className={styles["form__tags"]}>
+          {Object.values(MISC_TAGS).map((label, idx) => (
+            <Tag type="hash" key={idx}>
+              {label}
+            </Tag>
+          ))}
+        </div>
+      </div>
 
       <div className={styles["gradients-list"]}>
         {gradients.map((gradient, index) => (
