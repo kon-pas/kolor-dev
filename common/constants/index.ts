84be1a21c40cac3594efc7d8fa49dcdcf6faf9d8
kon-pas <konpas@mail.com>
2022-12-30 22:16:36 +0100

    feat: migrate to `prisma`

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -3,3 +3,5 @@ export { default as BRAND_COLORS } from "./brand-colors";
 export { default as TOAST_OPTIONS } from "./toast-options";
 
 export { default as MAIN_COLORS } from "./main-colors";
+
+export { default as MISC_TAGS } from "./misc-tags";
