2b77f410d1ba712ae55855718e006abf7988c7ed
kon-pas <konpas@mail.com>
2022-12-30 23:37:37 +0100

    init(`constants`): `main-colors-values`

--- common/constants/index.ts
+++ common/constants/index.ts
@@ -4,4 +4,6 @@ export { default as TOAST_OPTIONS } from "./toast-options";
 
 export { default as MAIN_COLORS } from "./main-colors";
 
+export { default as MAIN_COLORS_VALUES } from "./main-colors-values";
+
 export { default as MISC_TAGS } from "./misc-tags";
