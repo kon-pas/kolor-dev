b83c5dc518fd76ab222e57bf8c46f38bc045a0ab
kon-pas <konpas@mail.com>
2023-01-05 23:42:21 +0100

    feat(`IconSVG`): optional prop `viewBox`

--- common/components/elements/IconSVG/index.tsx
+++ common/components/elements/IconSVG/index.tsx
@@ -8,6 +8,7 @@ interface IconSVGProps {
   title?: string;
   desc?: string;
   filled?: boolean;
+  viewBox?: [number, number];
 }
 
 const IconSVG: FC<IconSVGProps> = ({
@@ -16,13 +17,14 @@ const IconSVG: FC<IconSVGProps> = ({
   title,
   desc,
   filled = false,
+  viewBox,
 }) => {
   return (
     <svg
       className={styles["icon-svg"]}
       xmlns="http://www.w3.org/2000/svg"
       fill={filled ? "currentColor" : "none"}
-      viewBox="0 0 24 24"
+      viewBox={viewBox ? `0 0 ${viewBox[0]} ${viewBox[1]}` : "0 0 24 24"}
       strokeWidth={strokeWidth ?? 1.75}
       stroke="currentColor"
       role="img"
