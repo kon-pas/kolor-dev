035fd4a21bdbd3bccdead70a5267ffe9c43a9578
kon-pas <konpas@mail.com>
2023-01-07 21:43:39 +0100

    refactor: add commnets

--- common/utils/get-main-color-hex.ts
+++ common/utils/get-main-color-hex.ts
@@ -1,6 +1,12 @@
 import type { MainColors } from "@enums";
 import { MAIN_COLORS_VALUES } from "@constants";
 
+// @@@ NOTE: THis is an overkill.
+
+/**
+ * @param colorName Name of the color in plain english.
+ * @returns The given color's hex code.
+ */
 const getMainColorHex = (colorName: MainColors): string =>
   MAIN_COLORS_VALUES[colorName];
 
