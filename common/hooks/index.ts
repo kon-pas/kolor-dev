3d62afbfe1df9427d5b94164cfd59f69287f2b36
kon-pas <konpas@mail.com>
2023-01-10 17:03:34 +0100

    init(`hooks`): `use-debounced-callback`

--- common/hooks/index.ts
+++ common/hooks/index.ts
@@ -1,3 +1,7 @@
-export { default as usePath } from "./use-path";
+export { default as usePathName } from "./use-path-name";
 
 export { default as useNavigation } from "./use-navigation";
+
+export { default as useDebounce } from "./use-debounce";
+
+export { default as useDebouncedCallback } from "./use-debounced-callback";
