7a0cb97ba1bf57b16d24251c6a99b0f78a77dba3
kon-pas <konpas@mail.com>
2023-01-14 22:59:21 +0100

    chore(`database`): adjust to new type names

--- common/database/gradients.ts
+++ common/database/gradients.ts
@@ -1,9 +1,9 @@
-// @@@ NOTE: This is a mess but almost never used.
+// @@@ NOTE: This is a mess but almost never used.g
 // As for now, its only purpose is to seed database and gets the job done.
 // See `@database/seed-database`.
 
 import type { GradientsJSON } from "@interfaces";
-import { MainColors, MiscTags } from "@enums";
+import { MainColor, MiscTag } from "@enums";
 import { customAlphabet } from "nanoid";
 
 const nanoid = customAlphabet(
@@ -16,208 +16,208 @@ export const gradients: GradientsJSON = [
     colors: ["#84ffc9", "#aab2ff", "#eca0ff"],
     title: "Faerie's Dream",
     tags: {
-      mainColors: [MainColors.Green, MainColors.Blue, MainColors.Purple],
-      misc: [MiscTags.Magic, MiscTags.Intense],
+      colors: [MainColor.Green, MainColor.Blue, MainColor.Purple],
+      misc: [MiscTag.Magic, MiscTag.Intense],
     },
   },
   {
     colors: ["#60efff", "#00ff87"],
     title: "Morning Breeze",
     tags: {
-      mainColors: [MainColors.Blue, MainColors.Green],
-      misc: [MiscTags.Intense],
+      colors: [MainColor.Blue, MainColor.Green],
+      misc: [MiscTag.Intense],
     },
   },
   {
     colors: ["#BFFF00", "#60F10A"],
     title: "Guilty Lime",
     tags: {
-      mainColors: [MainColors.Green],
-      misc: [MiscTags.Intense, MiscTags.Mono],
+      colors: [MainColor.Green],
+      misc: [MiscTag.Intense, MiscTag.Mono],
     },
   },
   {
     colors: ["#f89b29", "#ff0f7b"],
     title: "Ripe Mirabelle",
     tags: {
-      mainColors: [MainColors.Orange, MainColors.Pink],
-      misc: [MiscTags.Intense, MiscTags.Warm],
+      colors: [MainColor.Orange, MainColor.Pink],
+      misc: [MiscTag.Intense, MiscTag.Warm],
     },
   },
   {
     colors: ["#a9ff68", "#ff8989"],
     title: "Fresh Apple",
     tags: {
-      mainColors: [MainColors.Green, MainColors.Red],
-      misc: [MiscTags.Warm, MiscTags.Varied],
+      colors: [MainColor.Green, MainColor.Red],
+      misc: [MiscTag.Warm, MiscTag.Varied],
     },
   },
   {
     colors: ["#9bafd9", "#103783"],
     title: "Navy Blue",
     tags: {
-      mainColors: [MainColors.Blue],
-      misc: [MiscTags.Cold, MiscTags.Mono],
+      colors: [MainColor.Blue],
+      misc: [MiscTag.Cold, MiscTag.Mono],
     },
   },
   {
     colors: ["#f8f9c7", "#f2c1ea", "#f67b6b"],
     title: "Sweet Popsicle",
     tags: {
-      mainColors: [MainColors.Yellow, MainColors.Pink, MainColors.Orange],
-      misc: [MiscTags.Calm, MiscTags.Varied, MiscTags.Magic],
+      colors: [MainColor.Yellow, MainColor.Pink, MainColor.Orange],
+      misc: [MiscTag.Calm, MiscTag.Varied, MiscTag.Magic],
     },
   },
   {
     colors: ["#d3eef4", "#f1eec8", "#f3a46c"],
     title: "Wet Sand",
     tags: {
-      mainColors: [MainColors.Blue, MainColors.Yellow, MainColors.Orange],
-      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Varied],
+      colors: [MainColor.Blue, MainColor.Yellow, MainColor.Orange],
+      misc: [MiscTag.Light, MiscTag.Calm, MiscTag.Varied],
     },
   },
   {
     colors: ["#FFFF8B", "#f19e18", "#e62314"],
     title: "Red Fox's Tail",
     tags: {
-      mainColors: [MainColors.Yellow, MainColors.Orange, MainColors.Red],
-      misc: [MiscTags.Intense, MiscTags.Warm],
+      colors: [MainColor.Yellow, MainColor.Orange, MainColor.Red],
+      misc: [MiscTag.Intense, MiscTag.Warm],
     },
   },
   {
     colors: ["#fff95b", "#ff930f"],
     title: "Amber Gold",
     tags: {
-      mainColors: [MainColors.Yellow, MainColors.Orange],
-      misc: [MiscTags.Intense, MiscTags.Warm],
+      colors: [MainColor.Yellow, MainColor.Orange],
+      misc: [MiscTag.Intense, MiscTag.Warm],
     },
   },
   {
     colors: ["#f3f520", "#59d102"],
     title: "Freshly Cut Grass",
     tags: {
-      mainColors: [MainColors.Yellow, MainColors.Green],
-      misc: [MiscTags.Intense],
+      colors: [MainColor.Yellow, MainColor.Green],
+      misc: [MiscTag.Intense],
     },
   },
   {
     colors: ["#ebf4f5", "#b5c6e0"],
     title: "Cold Paper",
     tags: {
-      mainColors: [MainColors.White, MainColors.Gray],
-      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Cold, MiscTags.Mono],
+      colors: [MainColor.White, MainColor.Gray],
+      misc: [MiscTag.Light, MiscTag.Calm, MiscTag.Cold, MiscTag.Mono],
     },
   },
   {
     colors: ["#fbe9d7", "#f6d5f7"],
     title: "Warm Paper",
     tags: {
-      mainColors: [MainColors.Pink],
-      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Mono],
+      colors: [MainColor.Pink],
+      misc: [MiscTag.Light, MiscTag.Calm, MiscTag.Mono],
     },
   },
   {
     colors: ["#82f4b1", "#30c67c"],
     title: "Faded Lime",
     tags: {
-      mainColors: [MainColors.Green],
-      misc: [MiscTags.Cold, MiscTags.Mono],
+      colors: [MainColor.Green],
+      misc: [MiscTag.Cold, MiscTag.Mono],
     },
   },
   {
     colors: ["#751006", "#1f0021"],
     title: "Bloody Black",
     tags: {
-      mainColors: [MainColors.Red, MainColors.Black],
-      misc: [MiscTags.Dark, MiscTags.Mono],
+      colors: [MainColor.Red, MainColor.Black],
+      misc: [MiscTag.Dark, MiscTag.Mono],
     },
   },
   {
     colors: ["#00458e", "#000328"],
     title: "Oceans' Depths",
     tags: {
-      mainColors: [MainColors.Blue, MainColors.Black],
-      misc: [MiscTags.Dark, MiscTags.Cold, MiscTags.Mono],
+      colors: [MainColor.Blue, MainColor.Black],
+      misc: [MiscTag.Dark, MiscTag.Cold, MiscTag.Mono],
     },
   },
   {
     colors: ["#e4e95d", "#e89d1f"],
     title: "Shades of Lemon",
     tags: {
-      mainColors: [MainColors.Yellow],
-      misc: [MiscTags.Intense, MiscTags.Mono],
+      colors: [MainColor.Yellow],
+      misc: [MiscTag.Intense, MiscTag.Mono],
     },
   },
   {
     colors: ["#affcaf", "#12dff3"],
     title: "Hawaiian Shallows",
     tags: {
-      mainColors: [MainColors.Blue],
-      misc: [MiscTags.Intense, MiscTags.Cold, MiscTags.Mono],
+      colors: [MainColor.Blue],
+      misc: [MiscTag.Intense, MiscTag.Cold, MiscTag.Mono],
     },
   },
   {
     colors: ["#eef2f3", "#8399a2"],
     title: "Tabula Rasa",
     tags: {
-      mainColors: [MainColors.White, MainColors.Gray],
-      misc: [MiscTags.Light, MiscTags.Calm, MiscTags.Cold, MiscTags.Mono],
+      colors: [MainColor.White, MainColor.Gray],
+      misc: [MiscTag.Light, MiscTag.Calm, MiscTag.Cold, MiscTag.Mono],
     },
   },
   {
     colors: ["#c5f9d7", "#f7d486", "#f27a7d"],
     title: "Ising Model",
     tags: {
-      mainColors: [MainColors.Blue, MainColors.Yellow, MainColors.Red],
-      misc: [MiscTags.Varied],
+      colors: [MainColor.Blue, MainColor.Yellow, MainColor.Red],
+      misc: [MiscTag.Varied],
     },
   },
   {
     colors: ["#f8dadc", "#b6edc8", "#eeacdc"],
     title: "First Days of Spring",
     tags: {
-      mainColors: [MainColors.Pink, MainColors.Green, MainColors.Purple],
-      misc: [MiscTags.Light, MiscTags.Magic, MiscTags.Varied],
+      colors: [MainColor.Pink, MainColor.Green, MainColor.Purple],
+      misc: [MiscTag.Light, MiscTag.Magic, MiscTag.Varied],
     },
   },
   {
     colors: ["#9AEEA2", "#f7f065", "#FD9891"],
     title: "Middle of Summer",
     tags: {
-      mainColors: [MainColors.Green, MainColors.Yellow, MainColors.Red],
-      misc: [MiscTags.Intense, MiscTags.Varied],
+      colors: [MainColor.Green, MainColor.Yellow, MainColor.Red],
+      misc: [MiscTag.Intense, MiscTag.Varied],
     },
   },
   {
     colors: ["#e3e3e3", "#eb8a3d"],
     title: "Autumn's Peace",
     tags: {
-      mainColors: [MainColors.Gray, MainColors.Orange],
-      misc: [MiscTags.Calm],
+      colors: [MainColor.Gray, MainColor.Orange],
+      misc: [MiscTag.Calm],
     },
   },
   {
     colors: ["#eeddf3", "#FDB5BE", "#6b8080"],
     title: "Warmth of Winter",
     tags: {
-      mainColors: [MainColors.Pink, MainColors.Gray],
-      misc: [MiscTags.Intense, MiscTags.Varied],
+      colors: [MainColor.Pink, MainColor.Gray],
+      misc: [MiscTag.Intense, MiscTag.Varied],
     },
   },
   {
     colors: ["#c621e5", "#7d7cf9"],
     title: "Scent of Lavender",
     tags: {
-      mainColors: [MainColors.Purple],
-      misc: [MiscTags.Magic, MiscTags.Intense, MiscTags.Mono],
+      colors: [MainColor.Purple],
+      misc: [MiscTag.Magic, MiscTag.Intense, MiscTag.Mono],
     },
   },
   {
     colors: ["#000000", "#ffffff"],
     title: "Black & White",
     tags: {
-      mainColors: [MainColors.Black, MainColors.White],
-      misc: [MiscTags.Light, MiscTags.Dark],
+      colors: [MainColor.Black, MainColor.White],
+      misc: [MiscTag.Light, MiscTag.Dark],
     },
   },
 ]
