ca9bf5cbf07180b23644d1f774033128bd631903
kon-pas <konpas@mail.com>
2023-01-16 22:55:23 +0100

    refactor(`use-navigation`): clean comments up

--- common/hooks/use-navigation.ts
+++ common/hooks/use-navigation.ts
@@ -1,13 +1,11 @@
 import { useRouter } from "next/router";
-import { usePath } from "@hooks";
+import { usePathName } from "@hooks";
 
 // @@@ NOTE: Remove? After clean-up this hook is an overkill.
 const useNavigation = () => {
-  // const path = usePath();
   const { push } = useRouter();
   return {
     navigateTo: (url: string, pathname?: string) => {
-      // path.setName(pathname ?? url);
       push(url);
     },
   };
