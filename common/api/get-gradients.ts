9e34c5a05ce656ae14870a4f8602ac02afa81ab7
kon-pas <konpas@mail.com>
2023-01-16 22:48:37 +0100

    add(`get-gradients`): error handling

--- common/api/get-gradients.ts
+++ common/api/get-gradients.ts
@@ -1,7 +1,13 @@
 import type { GradientScheme } from "@types";
 import { prisma } from "@lib";
 
-const getGradients = async (): Promise<GradientScheme[]> =>
-  prisma.gradient.findMany();
+const getGradients = async (): Promise<GradientScheme[]> => {
+  try {
+    return await prisma.gradient.findMany();
+  } catch (err) {
+    console.error("Unable to find gradients", err);
+    return [];
+  }
+};
 
 export default getGradients;
