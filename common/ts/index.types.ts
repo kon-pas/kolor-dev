596dc66d15a98731d385aa369feb76baaeaaded5
kon-pas <konpas@mail.com>
2023-01-19 23:09:20 +0100

    add(`ts`): new direction-related types

--- common/ts/index.types.ts
+++ common/ts/index.types.ts
@@ -4,16 +4,8 @@ export type GradientHue = string[];
 
 export type GradientId = string;
 
-export type FourDirections = "top" | "right" | "bottom" | "left";
+export type FourDirections = "t" | "r" | "b" | "l";
 
-export type EightDirections =
-  | "top"
-  | "top right"
-  | "right"
-  | "bottom right"
-  | "bottom"
-  | "bottom left"
-  | "left"
-  | "top left";
+export type EightDirections = "t" | "tr" | "r" | "br" | "b" | "bl" | "l" | "tl";
 
 export type { Gradient as GradientScheme } from "@prisma/client";
