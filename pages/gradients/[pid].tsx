d25721ce97c3dd376dd74f119b10c9eefc157809
kon-pas <konpas@mail.com>
2023-01-19 18:49:30 +0100

    chore(`/gradients`): reorganize routes

---
+++ pages/gradients/[pid].tsx
@@ -0,0 +1,360 @@
+import styles from "@styles/pages/gradients/[pid].module.scss";
+import "react-toastify/dist/ReactToastify.css";
+
+import type { MiscTag, MainColor } from "@enums";
+import type { GradientScheme } from "@types";
+import type { NextPage, GetStaticProps, GetStaticPaths } from "next";
+
+import { NextRouter, withRouter } from "next/router";
+import { useState, useEffect } from "react";
+import ErrorPage from "next/error";
+import Head from "next/head";
+import { toast } from "react-toastify";
+import { ParsedUrlQuery } from "querystring";
+
+import { TOAST_OPTIONS } from "@constants";
+import { getRGB, isMiscTag, isMainColor } from "@utils";
+import { local } from "@services";
+import { getGradient, getGradients } from "@api";
+import { usePathName } from "@hooks";
+
+import TextUnderlined from "@components/elements/TextUnderlined";
+import Gradient from "@components/elements/GradientBackground";
+import Color from "@components/elements/ColorBackground";
+import IconSVG from "@components/elements/IconSVG";
+import Button from "@components/elements/Button";
+import CodeSnippet from "@components/elements/CodeSnippet";
+import Tag from "@components/elements/Tag";
+import SpanMonochrome from "@components/elements/SpanMonochrome";
+
+interface GradientPidProps {
+  statusCode: 404 | 500;
+  router: NextRouter;
+  gradient?: GradientScheme;
+}
+
+const GradientPid: NextPage<GradientPidProps> = ({
+  statusCode,
+  router,
+  gradient,
+}) => {
+  const [isSaved, setIsSaved] = useState<boolean>(false);
+
+  const { setPathName } = usePathName();
+
+  useEffect(() => {
+    setPathName("Gradient");
+  }, [setPathName]);
+
+  useEffect(() => {
+    setIsSaved(gradient ? local.gradients.includes(gradient.id) : false);
+  }, [gradient]);
+
+  if (statusCode === 404) return <ErrorPage statusCode={statusCode} />;
+
+  gradient = gradient as GradientScheme;
+
+  const handleReturnButtonOnClick = () => {
+    router.back();
+  };
+
+  const handleSaveButtonOnClick = () => {
+    const { id } = gradient as GradientScheme;
+    if (local.gradients.includes(id)) local.gradients.remove(id);
+    else local.gradients.add(id);
+    setIsSaved(isSaved => !isSaved);
+  };
+
+  const handleLinkButtonOnClick = () => {
+    navigator.clipboard.writeText(window.location.href).then(
+      () => {
+        toast("Link Copied to Clipboard", TOAST_OPTIONS);
+      },
+      () => {
+        toast("Copy to Clipboard Failed :/", TOAST_OPTIONS);
+      }
+    );
+  };
+
+  const handleImageButtonOnClick = () => {
+    const [width, height] = [1920, 1080];
+    const canvas = document.createElement("canvas") as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const linearGradient = ctx.createLinearGradient(
+      0,
+      height / 2,
+      width,
+      height / 2
+    );
+
+    gradient!.colors.forEach((color, idx, colors) => {
+      linearGradient.addColorStop(idx / (colors.length - 1), color);
+    });
+
+    [canvas.width, canvas.height] = [width, height];
+    ctx.fillStyle = linearGradient;
+    ctx.fillRect(0, 0, width, height);
+
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    const name = gradient!.title.replace(/[^a-z0-9]/gi, "").toLowerCase();
+
+    link.download = `${name}.png`;
+    link.href = image;
+    link.click();
+  };
+
+  // @@@ TODO: `/gradient/create`
+  const handleEditButtonOnClick = () => {
+    toast("Not Yet Available", TOAST_OPTIONS);
+  };
+
+  const handleColorOnCLick = (color: string) => {
+    navigator.clipboard.writeText(color).then(
+      () => {
+        toast(`Copied ${color}`, TOAST_OPTIONS);
+      },
+      () => {
+        toast("Copy to Clipboard Failed :/", TOAST_OPTIONS);
+      }
+    );
+  };
+
+  const handleCodeSnippetOnClick = (expr: string) => {
+    navigator.clipboard.writeText(expr).then(
+      () => {
+        toast("Snippet Copied to Clipboard", TOAST_OPTIONS);
+      },
+      () => {
+        toast("Copy to Clipboard Failed :/", TOAST_OPTIONS);
+      }
+    );
+  };
+
+  const handleTagOnClick = (tag: MiscTag | MainColor) => {
+    router.push({
+      pathname: "/gradients",
+      query: isMiscTag(tag)
+        ? {
+            misc: tag,
+          }
+        : isMainColor(tag)
+        ? {
+            colors: tag,
+          }
+        : undefined,
+    });
+  };
+
+  const codeSnippets = [
+    `${gradient.colors.map((color, idx) =>
+      idx === 0 ? color.toUpperCase() : " " + color.toUpperCase()
+    )}`,
+    `background: linear-gradient(${gradient.colors.map((color, idx) =>
+      idx === 0 ? color.toUpperCase() : " " + color.toUpperCase()
+    )});`,
+    `${gradient.colors.map((color, idx) =>
+      idx === 0 ? getRGB(color) : " " + getRGB(color)
+    )}`,
+    `background: linear-gradient(${gradient.colors.map((color, idx) =>
+      idx === 0 ? getRGB(color) : " " + getRGB(color)
+    )});`,
+  ];
+
+  return (
+    <>
+      <Head>
+        <title>Kolor Dev | {gradient.title}</title>
+      </Head>
+
+      <div className={styles["gradient-pid"]}>
+        <header className={styles["header"]}>
+          <div
+            className={styles["header__return"]}
+            onClick={handleReturnButtonOnClick}
+          >
+            <IconSVG>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3"
+              />
+            </IconSVG>
+          </div>
+
+          <h1 className={styles["header__heading"]}>
+            <TextUnderlined colors={gradient.colors}>
+              {gradient.title}
+            </TextUnderlined>
+          </h1>
+        </header>
+
+        <div className={styles["gradient"]}>
+          <Gradient colors={gradient.colors} />
+        </div>
+
+        <div className={styles["buttons"]}>
+          <div className={styles["buttons__left"]}>
+            <Button label="Save" onClick={handleSaveButtonOnClick}>
+              {isSaved ? (
+                <IconSVG filled>
+                  <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                </IconSVG>
+              ) : (
+                <IconSVG>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                  />
+                </IconSVG>
+              )}
+            </Button>
+          </div>
+
+          <div className={styles["buttons__right"]}>
+            <Button label="Link" onClick={handleLinkButtonOnClick}>
+              <IconSVG>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                />
+              </IconSVG>
+            </Button>
+
+            <Button label="Image" onClick={handleImageButtonOnClick}>
+              <IconSVG>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5M16.5 12L12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
+              </IconSVG>
+            </Button>
+
+            <Button label="Edit" onClick={handleEditButtonOnClick}>
+              <IconSVG>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                />
+              </IconSVG>
+            </Button>
+          </div>
+        </div>
+
+        <div className={styles["colors-list"]}>
+          {gradient.colors.map((color, idx, colors) => (
+            <div className={styles["colors-list__item"]} key={idx}>
+              <div
+                className={styles["colors-list__color"]}
+                onClick={() => handleColorOnCLick(color.toUpperCase())}
+              >
+                <Color hex={color}>
+                  <SpanMonochrome color={color}>
+                    {color.toUpperCase()}
+                  </SpanMonochrome>
+                </Color>
+              </div>
+
+              {idx + 1 !== colors.length && (
+                <div className={styles["colors-list__arrow"]}>
+                  <IconSVG>
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15.75 17.25L12 21m0 0l-3.75-3.75M12 21V3"
+                    />
+                  </IconSVG>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+
+        <section className={styles["code-snippets"]}>
+          {codeSnippets.map((code, idx) => (
+            <CodeSnippet
+              key={idx}
+              onClick={() => handleCodeSnippetOnClick(code)}
+            >
+              {code}
+            </CodeSnippet>
+          ))}
+        </section>
+
+        <div className={styles["tags"]}>
+          <div className={styles["tags__color-tags"]}>
+            {gradient.tags?.colors.map((color, idx) => (
+              <Tag
+                type="color"
+                color={color}
+                key={idx}
+                onClick={() => handleTagOnClick(color)}
+              >
+                {color}
+              </Tag>
+            ))}
+          </div>
+
+          <div className={styles["tags__hash-tags"]}>
+            {gradient.tags?.misc.map((label, idx) => (
+              <Tag
+                type="hash"
+                key={idx}
+                onClick={() => handleTagOnClick(label)}
+              >
+                {label}
+              </Tag>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const gradients: GradientScheme[] = await getGradients();
+
+  type Path = {
+    params: {
+      pid: string;
+    };
+  };
+
+  const paths: Path[] = gradients.map(({ id }) => ({
+    params: { pid: id },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  type Params = ParsedUrlQuery & {
+    pid: string;
+  };
+
+  const { pid } = params as Params;
+
+  const gradient: GradientScheme | null = await getGradient(pid);
+
+  if (typeof gradient === null) {
+    return {
+      props: {
+        statusCode: 404,
+      },
+    };
+  }
+
+  return {
+    props: {
+      gradient,
+      statusCode: 500,
+    },
+  };
+};
+
+export default withRouter(GradientPid);
---
+++ pages/gradients/index.tsx
@@ -0,0 +1,284 @@
+import styles from "@styles/pages/gradients/index.module.scss";
+
+import type { NextPage, GetServerSideProps } from "next";
+import type { NextRouter } from "next/router";
+import type { GradientScheme } from "@types";
+import type { MainColor, MiscTag } from "@enums";
+
+import { useEffect, useState, useCallback } from "react";
+import { withRouter } from "next/router";
+import Head from "next/head";
+import { clsx } from "clsx";
+
+import { getGradients } from "@api";
+import { usePathName } from "@hooks";
+import { getCleanString, isMiscTag, isMainColor } from "@utils";
+import { MISC_TAGS, MAIN_COLORS } from "@constants";
+import { local } from "@services";
+
+import GradientCard from "@components/pages/gradients/GradientCard";
+import TextUnderlined from "@components/elements/TextUnderlined";
+import Tag from "@components/elements/Tag";
+
+interface GradientsProps {
+  gradients: GradientScheme[];
+  router: NextRouter;
+}
+
+interface Filters {
+  name: string;
+  misc: MiscTag[];
+  colors: MainColor[];
+  liked: boolean;
+}
+
+// @@@ NOTE: SWC does not support TypeScript 4.9 yet.
+const initialFilters: Filters = {
+  name: "",
+  misc: [],
+  colors: [],
+  liked: false,
+};
+
+const Gradients: NextPage<GradientsProps> = ({ gradients, router }) => {
+  const [gradientsDisplayed, setGradientsDisplay] =
+    useState<GradientScheme[]>(gradients);
+
+  const [filters, setFilters] = useState<Filters>({ ...initialFilters });
+
+  const { setPathName } = usePathName();
+
+  useEffect(() => {
+    setPathName("Gradients");
+  }, [setPathName]);
+
+  useEffect(() => {
+    const { name, colors, misc } = router.query;
+
+    if (typeof name === "string") setFilters(prev => ({ ...prev, name }));
+
+    if (typeof colors === "string")
+      colors.split(",").forEach(color => {
+        if (isMainColor(color))
+          setFilters(prev => ({
+            ...prev,
+            colors: [...prev.colors, color],
+          }));
+      });
+
+    if (typeof misc === "string")
+      misc.split(",").forEach(tag => {
+        if (isMiscTag(tag))
+          setFilters(prev => ({
+            ...prev,
+            misc: [...prev.misc, tag],
+          }));
+      });
+  }, [router.query]);
+
+  useEffect(() => {
+    let filteredGradients = [...gradients];
+
+    filteredGradients = filteredGradients.filter(gradient =>
+      getCleanString(gradient.title).match(getCleanString(filters.name))
+    );
+
+    filters.colors.forEach((color: MainColor) => {
+      filteredGradients = filteredGradients.filter(gradient =>
+        gradient.tags?.colors.includes(color)
+      );
+    });
+
+    filters.misc.forEach((tag: MiscTag) => {
+      filteredGradients = filteredGradients.filter(gradient =>
+        gradient.tags?.misc.includes(tag)
+      );
+    });
+
+    if (filters.liked)
+      filteredGradients = filteredGradients.filter(gradient =>
+        local.gradients.includes(gradient.id)
+      );
+
+    setGradientsDisplay(filteredGradients);
+  }, [gradients, filters.colors, filters.misc, filters.name, filters.liked]);
+
+  // @@@ TODO: All those `useCallback`s may be an overkill since neither
+  // callbacks passed are computationally intensive nor children components are
+  // heavy
+
+  const handleNameSearch = useCallback((name: string) => {
+    setFilters(prev => ({
+      ...prev,
+      name,
+    }));
+  }, []);
+
+  const handleColorToggle = useCallback(
+    (color: MainColor, isIncluded: boolean) => {
+      setFilters(prev => ({
+        ...prev,
+        colors: isIncluded
+          ? prev.colors.filter(e => e !== color)
+          : [...prev.colors, color],
+      }));
+    },
+    []
+  );
+
+  const handleTagToggle = useCallback((tag: MiscTag, isIncluded: boolean) => {
+    setFilters(prev => ({
+      ...prev,
+      misc: isIncluded ? prev.misc.filter(e => e !== tag) : [...prev.misc, tag],
+    }));
+  }, []);
+
+  const handleLikeButton = useCallback(() => {
+    setFilters(prev => ({ ...prev, liked: !prev.liked }));
+  }, []);
+
+  const handleLuckyButton = useCallback(() => {
+    router.push(
+      `/gradient/${gradients[Math.floor(Math.random() * gradients.length)].id}`
+    );
+  }, [router, gradients]);
+
+  const handleResetButton = useCallback(() => {
+    setFilters({ ...initialFilters });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <title>Kolor Dev | Gradients</title>
+      </Head>
+
+      <div className={styles["gradients-page"]}>
+        <header className={styles["header"]}>
+          <h1 className={styles["header__heading-1"]}>{gradients.length}</h1>
+
+          <h2 className={styles["header__heading-2"]}>
+            <TextUnderlined thickness={16} offset={2}>
+              Gradients
+            </TextUnderlined>
+          </h2>
+
+          <p className={styles["header__paragraph"]}>
+            <span>Carefully selected for&nbsp;</span>
+
+            <span>Artists</span>
+
+            <span>,&nbsp;</span>
+
+            <span>Designes</span>
+
+            <span>&nbsp;&&nbsp;</span>
+
+            <span>Developers</span>
+
+            <span>.</span>
+          </p>
+        </header>
+
+        <div className={styles["form"]}>
+          <hr />
+
+          <input
+            className={clsx(styles["form__input"], styles["form__container"])}
+            type="search"
+            placeholder="Search by name"
+            value={filters.name}
+            onChange={event => handleNameSearch(event.target.value)}
+          />
+
+          <div className={styles["form__tags"]}>
+            {Object.values(MAIN_COLORS).map((color, idx) => {
+              const isActive: boolean = filters.colors.includes(color);
+              return (
+                <Tag
+                  type="color"
+                  color={color}
+                  key={idx}
+                  active={isActive}
+                  onClick={() => handleColorToggle(color, isActive)}
+                >
+                  {color}
+                </Tag>
+              );
+            })}
+          </div>
+
+          <div className={styles["form__tags"]}>
+            {Object.values(MISC_TAGS).map((tag, idx) => {
+              const isActive: boolean = filters.misc.includes(tag);
+
+              return (
+                <Tag
+                  type="hash"
+                  key={idx}
+                  active={isActive}
+                  onClick={() => handleTagToggle(tag, isActive)}
+                >
+                  {tag}
+                </Tag>
+              );
+            })}
+          </div>
+
+          {/* @@@ TODO: event handlers */}
+          <div
+            className={clsx(styles["form__buttons"], styles["form__container"])}
+          >
+            <button
+              className={clsx(
+                styles["form__button"],
+                filters.liked && styles["form__button--active"]
+              )}
+              onClick={handleLikeButton}
+            >
+              Show Liked
+            </button>
+
+            <button
+              className={styles["form__button"]}
+              onClick={handleLuckyButton}
+            >
+              Lucky Roll
+            </button>
+
+            <button
+              className={styles["form__button"]}
+              onClick={handleResetButton}
+            >
+              Reset Filters
+            </button>
+          </div>
+        </div>
+
+        {gradientsDisplayed.length !== 0 ? (
+          <div className={styles["gradients-list"]}>
+            {gradientsDisplayed.map((gradient, index) => (
+              <GradientCard key={index} gradient={gradient} />
+            ))}
+          </div>
+        ) : (
+          <div className={styles["gradients-info"]}>
+            Not a single gradient matches that :/
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const gradients = await getGradients();
+
+  return {
+    props: {
+      gradients,
+    },
+  };
+};
+
+export default withRouter(Gradients);
