fdb5e5598da869140ce804235a9d2ed96b6b68fd
kon-pas <konpas@mail.com>
2023-01-20 18:14:14 +0100

    wip(`pages`): `/gradients/custom`

--- pages/gradients/custom.tsx
+++ pages/gradients/custom.tsx
@@ -2,10 +2,9 @@
 // this page is a modified `/gradients/[pid]`. I wanted this to be SSR, but
 // `/gradients/[pid]` to stay SSG. I believe there is a better approach.
 
-import styles from "@styles/pages/gradients/[pid].module.scss";
+import styles from "@styles/pages/gradients/custom.module.scss";
 import "react-toastify/dist/ReactToastify.css";
 
-import type { MiscTag, MainColor } from "@enums";
 import type { NextPage, GetServerSideProps } from "next";
 import type { GradientHue } from "@types";
 
@@ -15,13 +14,13 @@ import Head from "next/head";
 import { toast } from "react-toastify";
 
 import { TOAST_OPTIONS } from "@constants";
-import { getRGB, isMiscTag, isMainColor } from "@utils";
+import { getRGB } from "@utils";
 import ErrorPage from "next/error";
 import { usePathName } from "@hooks";
 
 import TextUnderlined from "@components/elements/TextUnderlined";
 import Gradient from "@components/elements/GradientBackground";
-import Color from "@components/elements/ColorBackground";
+import BackgroundColor from "@components/elements/BackgroundColor";
 import IconSVG from "@components/elements/IconSVG";
 import Button from "@components/elements/Button";
 import CodeSnippet from "@components/elements/CodeSnippet";
@@ -38,14 +37,19 @@ interface CustomGradientScheme {
   title: string;
 }
 
+const MIN_COLORS: number = 2;
+const MAX_COLORS: number = 4;
+
 const GradientPid: NextPage<GradientPidProps> = ({
   statusCode,
   router,
   initialGradient,
 }) => {
   const { setPathName } = usePathName();
+
   const [currentGradient, setCurrentGradient] =
     useState<CustomGradientScheme>(initialGradient);
+
   const [newGradient, setNewGradient] =
     useState<CustomGradientScheme>(initialGradient);
 
@@ -86,15 +90,20 @@ const GradientPid: NextPage<GradientPidProps> = ({
       width,
       height / 2
     );
+
     currentGradient.colors.forEach((color, idx, colors) => {
       linearGradient.addColorStop(idx / (colors.length - 1), color);
     });
+
     [canvas.width, canvas.height] = [width, height];
+
     ctx.fillStyle = linearGradient;
     ctx.fillRect(0, 0, width, height);
+
     const image = canvas.toDataURL();
     const link = document.createElement("a");
     const name = currentGradient.title.replace(/[^a-z0-9]/gi, "").toLowerCase();
+
     link.download = `${name}.png`;
     link.href = image;
     link.click();
@@ -111,6 +120,29 @@ const GradientPid: NextPage<GradientPidProps> = ({
     );
   };
 
+  const handleRemoveColorOnClick = (idx: number) => {
+    if (newGradient.colors.length > MIN_COLORS) {
+      const tempGradient: CustomGradientScheme = { ...newGradient };
+      tempGradient.colors.splice(idx, 1);
+      setNewGradient(tempGradient);
+    } else toast(`Must have ${MIN_COLORS} colors at least`, TOAST_OPTIONS);
+  };
+
+  const handleEditColorOnClick = (color: string, idx: number) => {
+    const tempGradient: CustomGradientScheme = { ...newGradient };
+    tempGradient.colors[idx] = color;
+    setNewGradient(tempGradient);
+  };
+
+  const handleAddColorOnClick = () => {
+    if (newGradient.colors.length < MAX_COLORS)
+      setNewGradient(prev => ({
+        ...prev,
+        colors: [...prev.colors, prev.colors[prev.colors.length - 1]],
+      }));
+    else toast(`Can have ${MAX_COLORS} colors at most`, TOAST_OPTIONS);
+  };
+
   const handleCodeSnippetOnClick = (expr: string) => {
     navigator.clipboard.writeText(expr).then(
       () => {
@@ -120,21 +152,6 @@ const GradientPid: NextPage<GradientPidProps> = ({
         toast("Copy to Clipboard Failed :/", TOAST_OPTIONS);
       }
     );
-  };
-
-  const handleTagOnClick = (tag: MiscTag | MainColor) => {
-    router.push({
-      pathname: "/gradients",
-      query: isMiscTag(tag)
-        ? {
-            misc: tag,
-          }
-        : isMainColor(tag)
-        ? {
-            colors: tag,
-          }
-        : undefined,
-    });
   };
 
   const codeSnippets: any[] = [
@@ -245,18 +262,56 @@ const GradientPid: NextPage<GradientPidProps> = ({
         <div className={styles["colors-list"]}>
           {newGradient.colors.map((color, idx, colors) => (
             <div className={styles["colors-list__item"]} key={idx}>
-              <div
-                className={styles["colors-list__color"]}
-                onClick={() => handleColorOnCLick(color.toUpperCase())}
-              >
-                <Color hex={color}>
-                  <SpanMonochrome color={color}>
-                    {color.toUpperCase()}
-                  </SpanMonochrome>
-                </Color>
+              <div className={styles["colors-list__color"]}>
+                <BackgroundColor hex={color}>
+                  <div
+                    className={styles["colors-list__hex"]}
+                    onClick={() => handleColorOnCLick(color.toUpperCase())}
+                  >
+                    <SpanMonochrome color={color}>
+                      {color.toUpperCase()}
+                    </SpanMonochrome>
+                  </div>
+
+                  <div className={styles["colors-list__pane"]}>
+                    <div
+                      className={styles["colors-list__icon"]}
+                      onClick={() => handleRemoveColorOnClick(idx)}
+                    >
+                      <SpanMonochrome color={color}>
+                        <IconSVG>
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                        </IconSVG>
+                      </SpanMonochrome>
+                    </div>
+
+                    <div className={styles["colors-list__icon"]}>
+                      <SpanMonochrome color={color}>
+                        <IconSVG>
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"
+                          />
+                        </IconSVG>
+                      </SpanMonochrome>
+                      <input
+                        type="color"
+                        value={color}
+                        onChange={event =>
+                          handleEditColorOnClick(event.target.value, idx)
+                        }
+                      />
+                    </div>
+                  </div>
+                </BackgroundColor>
               </div>
 
-              {idx + 1 !== colors.length && (
+              {idx + 1 < MAX_COLORS && (
                 <div className={styles["colors-list__arrow"]}>
                   <IconSVG>
                     <path
@@ -269,6 +324,21 @@ const GradientPid: NextPage<GradientPidProps> = ({
               )}
             </div>
           ))}
+
+          {newGradient.colors.length < MAX_COLORS && (
+            <div
+              className={styles["colors-list__arrow"]}
+              onClick={handleAddColorOnClick}
+            >
+              <IconSVG>
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </IconSVG>
+            </div>
+          )}
         </div>
 
         <section className={styles["code-snippets"]}>
