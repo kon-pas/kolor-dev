6a9e6adfadff27c40e3def0bc5edc9c3414901da
kon-pas <konpas@mail.com>
2023-01-20 23:11:56 +0100

    feat(`get-clean-hex`): type guard added

--- common/utils/get-clean-hex.ts
+++ common/utils/get-clean-hex.ts
@@ -1,6 +1,7 @@
 import { HexColor } from "@types";
+import { isHexColor } from "@utils";
 
-const getCleanHex = (hex: HexColor): string =>
-  (hex[0] === "#" ? hex.slice(1) : hex).toUpperCase();
+const getCleanHex = (hex: HexColor | string): string =>
+  isHexColor(hex) ? (hex[0] === "#" ? hex.slice(1) : hex).toUpperCase() : hex;
 
 export default getCleanHex;
