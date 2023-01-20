ee10042f62ffbfdba18c0b7391acaa9146d80d96
kon-pas <konpas@mail.com>
2023-01-20 23:20:06 +0100

    chore: export utils

--- common/utils/index.ts
+++ common/utils/index.ts
@@ -14,6 +14,8 @@ export { default as getMainColorHex } from "./get-main-color-hex";
 
 export { default as getCleanString } from "./get-clean-string";
 
+export { default as getRandomHex } from "./get-random-hex";
+
 export { default as isMiscTagArr } from "./is-misc-tag-arr";
 
 export { default as isMainColorArr } from "./is-main-color-arr";
@@ -21,3 +23,5 @@ export { default as isMainColorArr } from "./is-main-color-arr";
 export { default as isMiscTag } from "./is-misc-tag";
 
 export { default as isMainColor } from "./is-main-color";
+
+export { default as isHexColor } from "./is-hex-color";
