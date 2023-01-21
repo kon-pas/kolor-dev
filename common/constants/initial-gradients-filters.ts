c3681e54087ba3c55092b3c226ee2666b7f3cdfc
kon-pas <konpas@mail.com>
2023-01-21 22:20:19 +0100

    init(`constants`): `initial-gradients-filters`

---
+++ common/constants/initial-gradients-filters.ts
@@ -0,0 +1,10 @@
+import type { GradientsFilters } from "@interfaces";
+
+const INITIAL_GRADIENTS_FITLERS: GradientsFilters = {
+  name: "",
+  misc: [],
+  colors: [],
+  liked: false,
+};
+
+export default INITIAL_GRADIENTS_FITLERS;
