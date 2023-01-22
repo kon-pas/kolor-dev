60b51bab59b1e468a110bc59b8a756f9907d1d00
kon-pas <konpas@mail.com>
2023-01-22 22:31:11 +0100

    init(`constants`): `initial-custom-gradient`

---
+++ common/constants/initial-custom-gradient.ts
@@ -0,0 +1,9 @@
+import type { CustomGradientScheme } from "@interfaces";
+
+const INITIAL_CUSTOM_GRADIENT: CustomGradientScheme = {
+  title: "Custom",
+  colors: ["#7F7F7F", "#7F7F7F", "#7F7F7F"],
+  direction: "to right",
+};
+
+export default INITIAL_CUSTOM_GRADIENT;
