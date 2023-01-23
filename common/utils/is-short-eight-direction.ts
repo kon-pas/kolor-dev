2afb48c657f38656ee26d40506510404c0023d21
kon-pas <konpas@mail.com>
2023-01-23 22:16:50 +0100

    fix(`utils`): `is-short-eight-direction`

--- common/utils/is-short-eight-direction.ts
+++ common/utils/is-short-eight-direction.ts
@@ -1,7 +1,7 @@
-import type { LongEightDirection } from "@types";
+import type { ShortEightDirection } from "@types";
 
 // @@@ TODO: Refactor to not repeat the code.
-const isLongEightDirection = (dir: string): dir is LongEightDirection =>
+const isShortEightDirection = (dir: string): dir is ShortEightDirection =>
   ["t", "tr", "r", "br", "b", "bl", "l", "tl"].includes(dir);
 
-export default isLongEightDirection;
+export default isShortEightDirection;
