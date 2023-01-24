4881226a9604f57f97378bfdc79a7f5056654f6d
kon-pas <konpas@mail.com>
2023-01-24 23:06:24 +0100

    init(`hooks`): `use-path`

---
+++ common/hooks/use-path.ts
@@ -0,0 +1,7 @@
+import { useContext } from "react";
+import { PathContextManager } from "@contexts";
+
+const usePath = () => useContext(PathContextManager.PathContext);
+
+
+export default usePath;
