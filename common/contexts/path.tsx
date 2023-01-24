a68f2807565c87f4e2c425e53df5e4ef6e805a95
kon-pas <konpas@mail.com>
2023-01-24 23:09:38 +0100

    feat(`contexts`): `path` redo

--- common/contexts/path.tsx
+++ common/contexts/path.tsx
@@ -1,28 +1,39 @@
 // @@@ TODO: Simplify this spaghetti.
 
 import type { ReactNode, FC } from "react";
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 
 // @@@ TODO: The `any` terrifies me.
-const PathNameContext = createContext<any>({});
+const PathContext = createContext<any>({});
 
-interface PathNameContextProviderProps {
+interface PathContextProviderProps {
   children: ReactNode;
 }
 
-const PathNameContextProvider: FC<PathNameContextProviderProps> = props => {
+const PathContextProvider: FC<PathContextProviderProps> = props => {
   const [pathName, setPathName] = useState<string>("");
+  const [pathUrl, setPathUrl] = useState<string>("/");
+
+  const setPath = (path: {
+    name: typeof pathName;
+    url: typeof pathUrl;
+  }): void => {
+    setPathName(path.name);
+    setPathUrl(path.url);
+  };
 
   return (
-    <PathNameContext.Provider value={{ pathName, setPathName }}>
+    <PathContext.Provider
+      value={{ pathName, setPathName, pathUrl, setPathUrl, setPath }}
+    >
       {props.children}
-    </PathNameContext.Provider>
+    </PathContext.Provider>
   );
 };
 
-const PathNameContextManager = {
-  PathNameContext,
-  PathNameContextProvider,
+const PathContextManager = {
+  PathContext,
+  PathContextProvider,
 };
 
-export default PathNameContextManager;
+export default PathContextManager;
