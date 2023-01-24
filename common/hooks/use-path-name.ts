9e68abd2dba0f85945bfe2ca281da5a282d721c2
kon-pas <konpas@mail.com>
2023-01-24 17:33:11 +0100

    chore(`hooks`): clean-up

--- common/hooks/use-path-name.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { PathNameContextManager } from "@contexts";
-
-const usePathName = () => useContext(PathNameContextManager.PathNameContext);
-
-export default usePathName;
