f09b80d6cf2fe75f41ef22c710301e130d1bc369
kon-pas <konpas@mail.com>
2023-01-24 23:17:28 +0100

    feat(`hooks`): remove `use-navigation` hook

--- common/hooks/index.ts
+++ common/hooks/index.ts
@@ -1,7 +1,5 @@
 export { default as usePath } from "./use-path";
 
-export { default as useNavigation } from "./use-navigation";
-
 export { default as useDebounce } from "./use-debounce";
 
 export { default as useDebouncedCallback } from "./use-debounced-callback";
