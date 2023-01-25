3b412183fa50dacc42a5aba02c2162bfaf8c8094
kon-pas <konpas@mail.com>
2023-01-25 21:29:41 +0100

    add(`Header`): adjustments to new `path` context

--- common/components/composition/Header/index.tsx
+++ common/components/composition/Header/index.tsx
@@ -4,7 +4,7 @@ import type { FC } from "react";
 import { useState, useEffect } from "react";
 import clsx from "clsx";
 import Image from "next/image";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
 import { usePath } from "@hooks";
 import IconSVG from "@components/elements/IconSVG";
@@ -73,12 +73,14 @@ const Header: FC = () => {
 
       <div className={styles["header__top"]}>
         <div className={styles["header__title"]}>
-          <span
-            className={styles["header__subtitle"]}
-            onClick={() => handleNavigation(path.url)}
-          >
-            {path.name !== "" ? path.name.toUpperCase() : null}
-          </span>
+          {path.name ? (
+            <span
+              className={styles["header__subtitle"]}
+              onClick={() => handleNavigation(path.url)}
+            >
+              {path.name.toUpperCase()}
+            </span>
+          ) : null}
         </div>
 
         <button
